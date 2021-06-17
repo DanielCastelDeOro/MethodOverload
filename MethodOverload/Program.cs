0702ce8b8b43c0d29a0b9d5070a0f8f861208e20
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-06-17 06:23:49 -0600

    Add project files.

---
+++ MethodOverload/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        public static int Add(int x, int y)
+           
+        {
+            return x + y;
+        }
+        public static decimal Add (decimal a, decimal b)
+        {
+            
+            return a + b;
+        }
+        public static string Add(int num1, int num2, bool isCheck)
+        {
+            var sum = num1 + num2;
+            if (isCheck == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCheck == true && sum == 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCheck == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            var x = 37;
+            var y = 55;
+
+            var answer = Add(x, y);
+
+            var a = 32.0m;
+            var b = 45.0m;
+
+            var decimalAnswer = Add(a, b);
+
+            var thirdAnswer = Add(1, 2, true);
+
+            Console.WriteLine($"int add: {answer} decimal add: {decimalAnswer}");
+            Console.WriteLine(thirdAnswer);
+        }
+    }
+}
